bfd853fa7b883d16742c53f2df984b6dbf563b19
Vlad Vamos <vladvamos@protonmail.com>
2024-05-07 20:07:40 +0200

    maximize window on launch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,9 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import viewmodel.AppViewModel
 
 val AppViewModel = compositionLocalOf<AppViewModel> {
@@ -15,6 +17,7 @@ fun main() = application {
     CompositionLocalProvider(AppViewModel provides viewModel) {
         Window(
             title = "Schaumamal",
+            state = rememberWindowState(placement = WindowPlacement.Maximized),
             onCloseRequest = {
                 viewModel.teardown()
                 exitApplication()
