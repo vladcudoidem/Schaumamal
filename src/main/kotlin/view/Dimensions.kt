eb384eb75548772187a40d11593945c60681b838
Vlad Vamos <vladvamos@protonmail.com>
2024-05-11 01:54:57 +0200

    start refactoring `view`

--- src/main/kotlin/view/Dimensions.kt
+++ src/main/kotlin/view/Dimensions.kt
@@ -3,11 +3,13 @@ package view
 import androidx.compose.ui.unit.dp
 
 object Dimensions {
+
     val smallPadding = 5.dp
     val mediumPadding = 10.dp
     val largePadding = 20.dp
 
     val paddingBetweenItems = 5.dp
+        // TODO isn't this just a padding?
 
     val smallCornerRadius = 5.dp
     val mediumCornerRadius = 10.dp
