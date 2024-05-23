8c697ce90941ce67637044348399756861e6ca04
Vlad Vamos <vladvamos@protonmail.com>
2024-05-24 00:04:34 +0200

    add density state

--- src/main/kotlin/view/MainScreen.kt
+++ src/main/kotlin/view/MainScreen.kt
@@ -1,18 +1,28 @@
 package view
 
+import AppViewModel
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import viewmodel.Colors.backgroundColor
+import androidx.compose.ui.platform.LocalDensity
 import view.button.ButtonLayer
 import view.panes.PaneLayer
 import view.screenshot.ScreenshotLayer
+import viewmodel.Colors.backgroundColor
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
+    val viewModel = AppViewModel.current
+    val density = LocalDensity.current.density
+
+    LaunchedEffect(density) {
+        viewModel.onNewDensity(density)
+    }
+
     Box(
         modifier = modifier
             .fillMaxSize()
