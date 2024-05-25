7bcdb26e30aa5f81f38a15d1633de019668187b9
Vlad Vamos <vladvamos@protonmail.com>
2024-05-25 12:15:46 +0200

    implement zoom

---
+++ src/main/kotlin/viewmodel/Values.kt
@@ -0,0 +1,11 @@
+package viewmodel
+
+@Suppress("MayBeConstant")
+object Values {
+
+    /* Screenshot Layer */
+
+    val zoomStep = 0.1f
+    val minScreenshotScale = 0.2f
+    val maxScreenshotScale = 3f
+}
