300735065ba2ff37a57da652e0892dcfa9b010ca
Vlad Vamos <vladvamos@protonmail.com>
2024-06-28 21:29:30 +0200

    change button appearance

--- src/main/kotlin/view/button/RoundButton.kt
+++ src/main/kotlin/view/button/RoundButton.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ButtonElevation
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,7 +15,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.unit.dp
-import shared.Colors.backgroundColor
+import shared.Colors.elevatedBackgroundColor
 import shared.Colors.primaryElementColor
 import shared.Dimensions.extractButtonDiameter
 import java.awt.Cursor
@@ -27,10 +28,17 @@ fun RoundIconButton(
     iconModifier: Modifier = Modifier,
     enabled: Boolean = true,
     buttonColors: ButtonColors = ButtonDefaults.buttonColors(
-        contentColor = backgroundColor,
-        disabledContentColor = backgroundColor.copy(alpha = 0.5f),
-        backgroundColor = primaryElementColor,
-        disabledBackgroundColor = primaryElementColor
+        contentColor = primaryElementColor,
+        disabledContentColor = primaryElementColor.copy(alpha = 0.35f),
+        backgroundColor = elevatedBackgroundColor,
+        disabledBackgroundColor = elevatedBackgroundColor
+    ),
+    buttonElevation: ButtonElevation = ButtonDefaults.elevation(
+        defaultElevation = 0.dp,
+        pressedElevation = 0.dp,
+        disabledElevation = 0.dp,
+        hoveredElevation = 0.dp,
+        focusedElevation = 0.dp
     )
 ) {
     Button(
@@ -39,6 +47,7 @@ fun RoundIconButton(
         shape = CircleShape,
         contentPadding = PaddingValues(0.dp),
         colors = buttonColors,
+        elevation = buttonElevation,
         modifier = buttonModifier
             .size(extractButtonDiameter)
             .pointerHoverIcon(PointerIcon(Cursor(Cursor.HAND_CURSOR)))
