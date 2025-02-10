44f8c2679d22aeab6f159ce85a40fc0dfc898511
Vlad Vamos <vladvamos@protonmail.com>
2025-02-10 19:17:02 +0100

    more changes 23

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     kotlin("jvm")
     id("org.jetbrains.compose")
     id("org.jetbrains.kotlin.plugin.compose")
-    kotlin("plugin.serialization") version "1.8.20"
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 group = "com.vladvamos.schaumamal"
