3aa7fbd1c74f06345145ccc75eb21bb9b80def0c
Vlad Vamos <vladvamos@protonmail.com>
2025-02-12 23:49:52 +0100

    small changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,9 +22,7 @@ dependencies {
     // compose.desktop.currentOs should be used in launcher-sourceSet
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
-    implementation(compose.desktop.currentOs) {
-        exclude(group = "org.jetbrains.compose.material")
-    }
+    implementation(compose.desktop.currentOs)
 
     implementation(libs.kotlinx.coroutinesCore)
     implementation(libs.kotlinx.coroutinesSwing)
