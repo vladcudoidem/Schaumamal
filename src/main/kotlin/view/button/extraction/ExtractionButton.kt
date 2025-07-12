48c096792d1d36f3a97289c587177441e38cbe40
Vlad Vamos <vladvamos@protonmail.com>
2025-07-12 16:42:02 +0200

    add icon to dump button

--- src/main/kotlin/view/button/extraction/ExtractionButton.kt
+++ src/main/kotlin/view/button/extraction/ExtractionButton.kt
@@ -1,18 +1,24 @@
 package view.button.extraction
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
-import java.awt.Cursor
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import shared.Colors.extractionButtonColor
 import shared.Dimensions.extractButtonDiameter
+import shared.times
+import java.awt.Cursor
 
 @Composable
 fun ExtractionButton(
@@ -20,23 +26,33 @@ fun ExtractionButton(
     onExtractButtonPressed: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val disabledBackgroundColor by
+    val buttonBackgroundColor = extractionButtonColor * 0.35f
+    val iconColor by
         animateColorAsState(
-            if (isExtractButtonEnabled) extractionButtonColor else extractionButtonColor.copy(0.3f)
+            if (isExtractButtonEnabled) extractionButtonColor else buttonBackgroundColor
         )
 
     Button(
         onClick = onExtractButtonPressed,
         enabled = isExtractButtonEnabled,
         shape = CircleShape,
+        elevation = ButtonDefaults.elevation(0.dp, 0.dp, 0.dp, 0.dp, 0.dp),
         colors =
             ButtonDefaults.buttonColors(
-                backgroundColor = extractionButtonColor,
-                disabledBackgroundColor = disabledBackgroundColor,
+                backgroundColor = buttonBackgroundColor,
+                disabledBackgroundColor = buttonBackgroundColor,
             ),
+        contentPadding = PaddingValues(2.dp),
         modifier =
             modifier
                 .size(extractButtonDiameter)
                 .pointerHoverIcon(PointerIcon(Cursor(Cursor.HAND_CURSOR))),
-    ) {}
+    ) {
+        Icon(
+            painter = painterResource("icons/shutter.svg"),
+            contentDescription = null,
+            tint = iconColor,
+            modifier = Modifier.fillMaxSize(),
+        )
+    }
 }
